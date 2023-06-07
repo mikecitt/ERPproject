0663b938d5e48afeeaf1d0f23a6e9245fb7fa021
DejanovicIT42 <73885609+DejanovicIT42@users.noreply.github.com>
2023-06-07 16:14:27 +0200

    neki komentar

--- ERP.TrgovinskaRadnja/TrgovinskaRadnja.Services/Services/PaymentService.cs
+++ ERP.TrgovinskaRadnja/TrgovinskaRadnja.Services/Services/PaymentService.cs
@@ -26,6 +26,7 @@ namespace TrgovinskaRadnja.Services.Services
             var intent = new PaymentIntent();
             var subtotal = basket.CartItems.Sum(item => item.Quantity * item.Product.Price);
             var deliveryFee = subtotal > 5000 ? 0 : 250;
+            //neki komentar
 
             if (string.IsNullOrEmpty(basket.PaymentIntentId))
             {
